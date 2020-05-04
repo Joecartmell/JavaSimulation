3c5ac8314f474b975c92e922612888bef360ff1c
JohnWCartmell <john.w.cartmell@gmail.com>
2020-05-04 21:54:32 +0100

    Debug scaling of graph and improve naming of variables.
    Improving naming makes it easier to see the bugs!

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -11,32 +11,37 @@ public class Graph extends JPanel {
         super.paintComponent(g);
         Graphics2D g1 = (Graphics2D) g;
         g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        int width = getWidth();
+        int width = getWidth(); 
+		System.out.println(width);
         int height = getHeight();
+		System.out.println(height);
         g1.draw(new Line2D.Double(mar, mar, mar, height - mar));
         g1.draw(new Line2D.Double(mar, height - mar, width - mar, height - mar));
-        double x = (double) (width - 2 * mar) / (10);
-        double scale = (double) (height - 2 * mar) / getMax();
+        double xscalefactor = (double) (width - 2 * mar) / (getMax_x_value());               // xscalefactor was previously named x
+        double yscalefactor = (double) (height - 2 * mar) / getMax_y_value();  // yscalefactor was previously named scale   //getMax_y_value was previously named getMax
         g1.setPaint(Color.BLUE);
 
-
         for (int i = 0; i < Population.xcoordinates.size(); i++) {
-            double x1 = mar + Population.xcoordinates.get(i) * x;
-            double y1 = height - mar - scale * Population.ycoordinates.get(i);
-            g1.draw(new Line2D.Double(x1, y1, (mar +Population.xcoordinates.get(i) * x) , (height - mar - scale * Population.ycoordinates.get(i)) + 20));
+            double x1 = mar + Population.xcoordinates.get(i) * xscalefactor;
+            double y1 = height - mar - yscalefactor * Population.ycoordinates.get(i);
+			System.out.printf("plotting x at : %f, plotting y at %f\n", x1,y1); 
+            g1.draw(new Line2D.Double(x1, y1, x1 , y1 + 20));
             g1.fill(new Ellipse2D.Double(x1 - 2, y1 - 2, 4, 4));
         }
 
-
+    }
+	
+	private int getMax_x_value() { 
+        return 10;
     }
 
-    private int getMax() {
+    private int getMax_y_value() {
         int max = -Integer.MAX_VALUE;
         for (int i = 0; i < Population.xcoordinates.size(); i++) {
-            if (Population.xcoordinates.get(i) > max)
-                max = Population.xcoordinates.get(i);
-
+            if (Population.ycoordinates.get(i) > max)   // Ooops - this needs say ycoordinates as well
+                max = Population.ycoordinates.get(i);  //previously  Population.xcoordinates.get(i) !!!!!!!!!!!!!
         }
+		System.out.printf("max is %d\n",max);
         return max;
     }
 
